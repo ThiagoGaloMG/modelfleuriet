70a5673844499aab7ff821c238b629cc9b9ab992
ThiagoGaloMG <91726223+ThiagoGaloMG@users.noreply.github.com>
2025-07-10 19:41:17 -0300

    Update valuation_analysis.py

--- core/valuation_analysis.py
+++ core/valuation_analysis.py
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore")
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt=\'%Y-%m-%d %H:%M:%S\'
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
 BASE_DIR = Path(__file__).resolve().parent
