68a8f7d60a97a08148a8660885b3476034a214e4
ThiagoGaloMG <91726223+ThiagoGaloMG@users.noreply.github.com>
2025-07-15 12:59:48 -0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,5 @@
-# Módulo core para análises Fleuriet e Valuation
+# modelfleuriet/core/__init__.py
+
+# Este arquivo torna o diretório 'core' um pacote Python.
+# Pode ser usado para inicializações de pacotes ou configurações globais.
 
